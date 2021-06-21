6f0395ec0470ac82538619de2bda7891f3569388
LanghaniKuldeep <langhanikuldeep@gmail.com>
2021-06-21 15:08:43 -0400

    Scenario

---
+++ godog/sms/sms.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"e2e-testing/internal/config"
+	d "e2e-testing/pkg/domains"
+	"e2e-testing/pkg/ports/calls"
+)
+
+var Configuration config.ConfigType
+var SecondaryPort calls.SecondaryPort
+var PrimaryPort calls.PrimaryPort
+var ResponseSMS d.ResponseSMS
+var Ch = make(chan string)
+
+func main() {
+
+}
