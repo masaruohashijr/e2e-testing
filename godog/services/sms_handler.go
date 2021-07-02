2325b4298de249b0f8bc450d3e694f415a2f8d13
LanghaniKuldeep <langhanikuldeep@gmail.com>
2021-07-02 09:30:17 -0400

    SMS scenarios Fix

---
+++ godog/services/sms_handler.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func SmsHandler(w http.ResponseWriter, r *http.Request) {
+	println("GatherHandler")
+	xml, err := os.ReadFile("../../xml/sms.xml")
+	if err != nil {
+		println(err.Error())
+	}
+	println(string(xml))
+	w.Write([]byte(xml))
+}
+
+func SmsStatusHanlder(w http.ResponseWriter, r *http.Request) {
+	println("*********** SMS Status Callback")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		println(err.Error())
+	}
+	b := string(body)
+	println(b)
+	if strings.Contains(b, "DlrStatus=delivered") {
+		Ch <- b
+	}
+}
